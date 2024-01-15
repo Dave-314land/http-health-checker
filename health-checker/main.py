ed32a66e64a4396803d02570442c05c685840491
Dave-314land <dave.l.piland@gmail.com>
2024-01-15 16:19:27 -0500

    load endpoints from yaml file

--- health-checker/main.py
+++ health-checker/main.py
@@ -1,23 +1,41 @@
-import typer
-import requests
 import time
 from datetime import timedelta
 
+import requests
+import typer
+import yaml
 
-urls = [
-    'https://fetch.com/',
-    'https://fetch.com/careers',
-    'https://fetchrewards.com/'
-]
+
+yamlFile = 'urls.yaml'
+
+
+def get_endpoints_from_file(urlFile):
+    with open(urlFile, 'r', encoding='utf-8') as file:
+        data = yaml.full_load(file)
+        endpoints = []
+        for row in data:
+            endpoint = {
+                'headers': row.get('headers'),
+                'method': row.get('method'),
+                'name': row.get('name'),
+                'url': row.get('url'),
+                'body': row.get('body')
+            }
+
+            endpoints.append(endpoint)
+        return endpoints
 
 
 def return_endpoint_status():
-    for url in urls:
+    endpoints = get_endpoints_from_file(yamlFile)
+    for endpoint in endpoints:
+        url = endpoint.get('url')
+        name = endpoint.get('name')
         response = requests.get(url)
         if response.status_code and response.elapsed < timedelta(microseconds=500000):
-            print('UP')
+            print(f'{name} is UP')
         else:
-            print('DOWN')
+            print(f'{name} is DOWN')
 
 
 def run_health_checker():
